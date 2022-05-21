08a17ecceb885666b604e603fe2397d9ad14515a
juliantapia <julytapia_05@hotmail.com>
2022-05-21 10:51:29 -0300

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -2,8 +2,7 @@ import { getData } from "./getData.js";
 import { Prestamo } from "./clases.js";
 
 document.getElementById('formTask').addEventListener('submit', savePrestamo);
-function savePrestamo(e){   
-  
+function savePrestamo(e){ 
     let monto = document.getElementById('monto').value;
     let cuotas = document.getElementById('cuotas').value;
     let interes = 0.03;
@@ -11,16 +10,12 @@ function savePrestamo(e){
     let montoCompuesto = 0;
     let amortMensual = 0;
     let saldo = 0; 
-
     const pasarVariable = new Prestamo(monto,cuotas,interes,amort,montoCompuesto,amortMensual,saldo);
-
     const resultado = pasarVariable.amortizacion();    
     const MC = pasarVariable.amortizacionCapital();  
     const AM = pasarVariable.amortizacionMensual(resultado,MC);
-    const saldoResult = pasarVariable.saldoCalculo(monto,AM);   
-    
+    const saldoResult = pasarVariable.saldoCalculo(monto,AM);
     const task = new Prestamo(monto,cuotas,interes,resultado,MC,AM, saldoResult );   
-    
     if(camposCorrectos(task)) {
         let tasks = localStorage.getItem('tasks');
         tasks = tasks === null && [] ;        
@@ -32,40 +27,34 @@ function savePrestamo(e){
             tasks.push(task);                    
             localStorage.setItem('tasks',JSON.stringify(tasks))
         }
-      }     
-        
+    }        
     getTasks();
     e.preventDefault();
 }
-
 const Tasas = async () =>{  
   const MostarTasas = await getData();
   MostarTasas.forEach(tasas => {
-  let tasasView = document.getElementById('tasas');  
-  tasasView.innerHTML += `
-                    <tbody>
-                      <tr class="table-info">
-                        <td>TNA:</td>
-                        <td>${tasas.TNA} %</td>                       
-                      </tr> 
-                      <tr class="table-info">
-                        <td>CFT:</td>
-                        <td>${tasas.CFT} %</td>                       
-                      </tr> 
-                      <tr class="table-info">
-                        <td>TEA:</td>
-                        <td>${tasas.TEA} %</td>                       
-                      </tr>  
-                      <tr class="table-info">
-                        <td>CFTNA:</td>
-                        <td>${tasas.CFTNA} %</td>                       
-                      </tr>
-                      <tr class="table-info">
-                        <td>TEM:</td>
-                        <td>${tasas.TEM} %</td>                       
-                      </tr>                                           
-                    </tbody>  
-        `;
+    let tasasView = document.getElementById('tasas');  
+    tasasView.innerHTML += ` <table class="table" ">
+                            <thead>
+                              <th>TNA</th>
+                              <th>CFT</th>
+                              <th>TEA</th>
+                              <th>CFTNA</th>
+                              <th>TEM</th>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>${tasas.TNA} %</td>                       
+                                <td>${tasas.CFT} %</td>                 
+                                <td>${tasas.TEA} %</td>                       
+                                <td>${tasas.CFTNA} %</td>                       
+                                <td>${tasas.TEM} %</td>   
+                              </tr>                                                      
+                          </tbody>
+                          </table>  
+                          <p>${tasas.Descripcion}</p>`;
+                          
   })
 }
 
@@ -75,44 +64,41 @@ function getTasks(){
     tasksView.innerHTML = ''; 
     tasks.forEach(function(tasks){    
       let monto = tasks.monto;   
-      tasksView.innerHTML += `<div class="mb-3 alert alert-success"  >
-      <table id="lista-tabla" class="table">
-                    <thead>
-                        <tr>
-                            <th>Monto</th>
-                            <th>años</th>
-                            <th>Capital</th>
-                            <th>Interés</th>
-                            <th>Amortizacion</th>
-                            <th>Saldo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                    <td>${tasks.monto} </td>                 
-                    <td>${tasks.cuotas} </td>                       
-                    <td>${tasks.amort} <t/d>                       
-                    <td>${tasks.montoCompuesto} </td>
-                    <td>${tasks.amortMensual} </td>
-                    <td>${tasks.saldo} </td>
-                   </tr>
-                   </tbody>
-                   </table>
-    </div>`;       
-                
-                   
-       const botonBorrar = document.createElement("button");
-       botonBorrar.innerText= "Borrar";
-       botonBorrar.className = "btn btn-danger";
-       botonBorrar.addEventListener("click", () => {
-         deleteTask(monto);
-       })
-       tasksView.appendChild(botonBorrar);    
-       const br = document.createElement("br");
-       br.innerText  
-    });
+      tasksView.innerHTML += `<br>
+                              <div class="mb-3 alert alert-success"  >
+                                <table id="lista-tabla" class="table">
+                                                  <thead>
+                                                      <tr>
+                                                          <th>Monto Solicitado</th>
+                                                          <th>Años</th>
+                                                          <th>Capital</th>
+                                                          <th>Interés</th>
+                                                          <th>Amortizacion</th>
+                                                          <th>Saldo Restante</th>
+                                                      </tr>
+                                                  </thead>
+                                                  <tbody>
+                                                  <tr>
+                                                  <td>${tasks.monto} $</td>                 
+                                                  <td>${tasks.cuotas} </td>                       
+                                                  <td>${tasks.amort} $<t/d>                       
+                                                  <td>${tasks.montoCompuesto} $</td>
+                                                  <td>${tasks.amortMensual} $</td>
+                                                  <td>${tasks.saldo} $</td>
+                                                </tr>
+                                                </tbody>
+                                    </table>
+                                </div>`;              
+      const botonBorrar = document.createElement("button");
+      botonBorrar.innerText= "Borrar";
+      botonBorrar.className = "btn btn-danger";
+      botonBorrar.addEventListener("click", () => {deleteTask(monto);})
+      tasksView.appendChild(botonBorrar);    
+      const br = document.createElement("br");
+      tasksView.appendChild(br);
 
-  
+      br.innerText  
+    });  
 }
 
 function deleteTask(monto) {
